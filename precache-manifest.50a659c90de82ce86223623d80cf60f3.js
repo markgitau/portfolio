27f14f982fc8e279d1ce8f3df70214a32ccef0cf
Mark Gitau 23 <markgitau@flume.cs.dartmouth.edu>
2020-07-23 15:27:52 -0400

    Updates

---
+++ precache-manifest.50a659c90de82ce86223623d80cf60f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132ae1147bf1eb00582e1f02b9ebfb9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2115afbaff74458a10d",
+    "url": "/static/css/main.69d7cbbd.chunk.css"
+  },
+  {
+    "revision": "5bea6485ba5596232ff7",
+    "url": "/static/js/2.a505de7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a505de7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2115afbaff74458a10d",
+    "url": "/static/js/main.b2d49fcb.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
